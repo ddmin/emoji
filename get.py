db5f37e2923c342800cc1d8cd7ec11a330df56d0
ddmin <ddmin216@outlook.com>
2021-07-15 14:54:23 -0400

    🫓 emoji

--- get.py
+++ get.py
@@ -1,4 +1,28 @@
-# Create emoji list by scraping unicode.org
+# Create emoji list by scraping unicode website
 
-#  <span lang="1st place medal"> 🥇 :1st place medal</span>
-# <span lang="{emoji_name} {emoji_symbol} :{emoji_name}</span>
+import requests
+import time
+
+UNICODE_LIST = 'https://unicode.org/Public/emoji/13.1/emoji-test.txt'
+EMOJI_FILE = './emoji-list'
+
+txt = requests.get(UNICODE_LIST).text
+
+with open(EMOJI_FILE, 'w') as f:
+    f.write('')
+
+# getting the emojis
+for line in txt.split('\n'):
+    if len(line) > 0 and line[0] == '#':
+        pass
+    else:
+        s = line.split(';')
+        if len(s) > 1:
+            emoji_info = s[1].split('# ')[1].split()
+            emoji_name = " ".join(emoji_info[2:]).replace(':', '')
+            emoji_symbol = emoji_info[0]
+
+            formatted = f'<span lang="{emoji_name.lower()}"> {emoji_symbol} :{emoji_name}</span>'
+
+            with open(EMOJI_FILE, 'a') as f:
+                f.write(formatted + '\n')
