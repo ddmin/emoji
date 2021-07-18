f38dd665b9e5542d998eafd6136695dbb93a0304
ddmin <ddmin216@outlook.com>
2021-07-18 15:31:08 -0400

    🫑 categories⁉️

--- get.py
+++ get.py
@@ -3,6 +3,15 @@
 import requests
 import time
 
+def sanitize(string):
+    new_string = ''
+    for letter in string:
+        if letter.isalpha() or letter == " ":
+            new_string += letter
+        else:
+            new_string += '-'
+    return new_string
+
 UNICODE_LIST = 'https://unicode.org/Public/emoji/13.1/emoji-test.txt'
 EMOJI_FILE = './emoji-list'
 
@@ -12,17 +21,24 @@ with open(EMOJI_FILE, 'w') as f:
     f.write('')
 
 # getting the emojis
-for line in txt.split('\n'):
-    if len(line) > 0 and line[0] == '#':
-        pass
-    else:
-        s = line.split(';')
-        if len(s) > 1:
+for line in txt.split('# subgroup: '):
+    l = line.split('\n')
+
+    # get emoji category
+    emoji_category = l.pop(0)
+    emoji_category.replace(':', '')
+
+    for emoji in l:
+        if not emoji or emoji[0] == "#":
+            pass
+        else:
+            s = emoji.split(';')
+
+            # get emoji details
             emoji_info = s[1].split('# ')[1].split()
             emoji_name = " ".join(emoji_info[2:]).replace(':', '')
             emoji_symbol = emoji_info[0]
 
-            formatted = f'<span lang="{emoji_name.lower()}"> {emoji_symbol} :{emoji_name}</span>'
-
+            formatted = f'<span lang="{emoji_name.lower()}-{sanitize(emoji_category.lower())}"> {emoji_symbol} :{emoji_name}</span>'
             with open(EMOJI_FILE, 'a') as f:
                 f.write(formatted + '\n')
